2197e00cb8022a5d224ae0a9b3a1deb4d6c010ac
sduartecor <santiagoduartecor@outlook.com>
2023-01-11 17:18:19 +0000

    Version final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
 
 	const [tarea, setTarea] = useState("");
 	const [lista, setLista] = useState([]);
+	const [mensaje, setMensaje] = useState("");
 
 	function addTarea(e) {
 		if(e.key==='Enter'){
@@ -15,16 +16,23 @@ const Home = () => {
 			if(tarea == "") {
 				alert("Ingresa una tarea")
 			} else{
-				setLista([...lista, tarea]); 
+				setLista([...lista, tarea]); 	
 				setTarea("");
+				setMensaje("visually-hidden")
 			}
 		}
 	}
 
 	function removeTarea(id) {
-		setLista(lista.filter((tarea, index) => index !== id)) 
+
+			setLista(lista.filter((tarea,index) => index !== id)) ;
+			if (lista.length === 1) {
+				setMensaje("");
+			}
+		
 	}
 
+	
 	return (
 		<div>
 			<h1 className="text-secondary fw-light text-center mt-3" style={{fontSize: "80px"}}>todos</h1>
@@ -32,10 +40,10 @@ const Home = () => {
 
 			<ul class="list-group list-group-flush shadow-lg bg-body-tertiary rounded w-50 rounded-0  fw-light">
 
-  <li class="list-group-item" aria-current="true"><input className="form-control fs-5 fw-light rounded-0 border border-0" type="text" placeholder="Ingrese una tarea" aria-label="Disabled input example" value={tarea} onChange={(e) => setTarea(e.target.value)} onKeyDown={addTarea} required/></li>
+  <li class="list-group-item" aria-current="true"><input className="form-control fs-5 fw-light rounded-0 border border-0 px-3" type="text" placeholder="What needs to be done?" aria-label="Disabled input example" value={tarea} onChange={(e) => setTarea(e.target.value)} onKeyDown={addTarea} required/></li>
 
-  {lista.map((tarea, id) => <li className="list-group-item fs-5" key={id}>{tarea}<button type="button" className="btn btn-outline-light border-0 float-end" onClick={() => removeTarea(id)}>X</button></li> )}
-  
+  {lista.map((tarea, id) => <li className="list-group-item fs-5 px-5"  key={id}>{tarea}<button type="button" className="btn btn-outline-light border-0 float-end"  onClick={() => removeTarea(id)}>X</button></li> )}
+  <li class={"list-group-item disabled fs-5 px-5 " + mensaje} aria-disabled="true">No hay tareas, añadir tareas</li>
   <li class="list-group-item disabled" aria-disabled="true">{lista.length} item left</li>
 </ul>
 			
